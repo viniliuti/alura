76088b563e95b02bcd02d3be383d83e37a50fc61
Vinicio Liuti <viniliuti@live.com>
2020-07-20 21:43:01 -0300

    last class & course finish

---
+++ csharp/formacao.Net/parte2/06-ByteBank/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _06_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(864, 32114);
+
+            Console.WriteLine(conta.Agencia);
+            Console.WriteLine(conta.Numero);
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+
+            ContaCorrente conta2 = new ContaCorrente(864, 32115);
+
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+        }
+    }
+}
