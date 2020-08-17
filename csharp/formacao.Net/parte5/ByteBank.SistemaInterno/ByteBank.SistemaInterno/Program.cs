fc8dae35581327358e691f68b4d4549f50993bcc
Vinicio Liuti <viniliuti@live.com>
2020-08-16 21:22:35 -0300

    separating the project in folders + end of class 3

---
+++ csharp/formacao.Net/parte5/ByteBank.SistemaInterno/ByteBank.SistemaInterno/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ByteBank.Modelos;
+
+namespace ByteBank.SistemaInterno
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ContaCorrente conta = new ContaCorrente(321, 123);
+
+			System.Console.WriteLine(conta.Saldo);
+		}
+	}
+}
