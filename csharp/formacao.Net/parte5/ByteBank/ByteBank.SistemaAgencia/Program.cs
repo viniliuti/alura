e09e02f611fda698c099e5dc60ed0a45f32368d6
Vinicio Liuti <viniliuti@live.com>
2020-11-22 16:31:38 -0300

    regex finishing part6 class 4

--- csharp/formacao.Net/parte5/ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ csharp/formacao.Net/parte5/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using ByteBank.Modelos;
 
 namespace ByteBank.SistemaAgencia
@@ -13,12 +14,29 @@ namespace ByteBank.SistemaAgencia
 
 			// System.Console.WriteLine(conta.Numero);
 
+
+			GetPhoneNumber();
+		}
+
+		private static void GetValueFromURL()
+		{
 			string url = "pagina?moedaOrigem=Real&moedaDestINo=doLar&valor=1500";
 			ExtratorValorDeArgumentosURL valorURL = new ExtratorValorDeArgumentosURL(url);
 
 			System.Console.WriteLine(valorURL.GetValor("moedaDestino"));
 			System.Console.WriteLine(valorURL.GetValor("moedaOrigem"));
 			System.Console.WriteLine(valorURL.GetValor("Valor"));
+		}
+
+		private static void GetPhoneNumber()
+		{
+			string padrao = "[0-9]{4,5}-*[0-9]{4}";
+
+			string texto = "4587-9856 Meu nome é Vinicio, me ligue em 4587-9856";
+
+			var bIsMatch = Regex.Match(texto, padrao);
+
+			System.Console.WriteLine(bIsMatch);
 		}
 	}
 }
